056e763a1780a82639afa851fbdae56dbe7e3c0a
Honza Zrec <honzazrec@Honzas-MacBook-Pro.local>
2025-12-24 14:47:48 +0100

    Vrácení mapy závad do sidebaru

--- js/main.js
+++ js/main.js
@@ -525,7 +525,7 @@ window.addEventListener("DOMContentLoaded", async () => {
   const zavadyView = document.getElementById("zavadyView");
   const zavadyList = document.getElementById("zavadyList");
 
-  const mapCategories = ["kose", "lampy", "kontejnery", "zelen", "mapa"];
+  const mapCategories = ["kose", "lampy", "kontejnery", "zelen", "mapa", "zavady-mapa"];
 
   const greenspaceVisibility = { trava: true, zahony: true };
   const mapLayersVisibility = { lampy: false, kose: true, zavady: true, zelen: false };
